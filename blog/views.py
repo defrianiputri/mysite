67e0cd815ac5bdf18dc1fcea8451482bde849d4b
Defriani <defrianiputri@gmail.com>
2018-10-01 18:38:56 +0700

    project django

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def main_dashboard(request, *args, **kwargs):
+    print(args, kwargs) # request to know user 
+    print(request.user) # knowing user who accesed the page 
+    # return HttpResponse("Welcome to my blog dashboard") 
+    return render (request, "base.html", {})
